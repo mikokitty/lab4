b56061603b87c45bf787fbf96a0f339b5a98fa8e
mikokitty <jlxie@ucsd.edu>
2016-01-28 10:44:42 -0800

    final

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 // Example route
-// var user = require('./routes/user');
+//var user = require('./routes/user');
+
+var project = require('./routes/project');
 
 var app = express();
 
@@ -36,6 +38,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project',project.viewProject);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
